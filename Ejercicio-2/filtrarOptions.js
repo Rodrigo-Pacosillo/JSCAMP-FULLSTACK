2cf70583ec70a5456a3daa1c47d148482872a4a4
Rodry555 <fisicacalculoalgebra@gmail.com>
2026-08-21 23:49:25 +0000

    prueba de fetch

--- Ejercicio-2/filtrarOptions.js
+++ Ejercicio-2/filtrarOptions.js
@@ -19,3 +19,30 @@ filtro.addEventListener("change",function(event){
 
 })
 
+
+const ContenedorDeJobs = document.querySelector(".jobs-listings")
+
+async function recuperarData(){
+    const datos = await fetch("./data.json")
+    const objetos = await datos.json()
+
+    objetos.forEach((objeto)=>{
+        const article = document.createElement('article')
+        article.classList.add("job-listing-card")
+        
+        //article.setAttribute("data-modalidad", objeto.data.modalidad)
+        article.dataset.modalidad = objeto.data.modalidad
+        
+        article.innerHTML=`<div>
+        <h3>${objeto.titulo}</h3>
+        <small>${objeto.empresa} | ${objeto.ubicacion}</small>
+        <p>${objeto.descripcion}</p>
+        </div>
+        <button type="submit" class="button-apply-job">enviar</button>`
+        
+        ContenedorDeJobs.appendChild(article)
+    })
+}
+
+await recuperarData()
+console.log("algoooo")
